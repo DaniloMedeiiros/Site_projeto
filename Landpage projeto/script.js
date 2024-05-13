ea440fdd5a64e136c2c17c309e742abd48054789
Mateus <mateuschgs.o@gmail.com>
2024-05-13 19:31:33 -0300

    Site inicio

---
+++ Landpage projeto/script.js
@@ -0,0 +1,9 @@
+function showServicos() {
+    document.getElementById('orcamentoBox').style.transform = 'translateX(300px)';
+    document.getElementById('servicosBox').style.transform = 'translateX(300px)';
+}
+
+function showOrcamento() {
+    document.getElementById('orcamentoBox').style.transform = 'translateX(0)';
+    document.getElementById('servicosBox').style.transform = 'translateX(0)';
+}
