2199f8a69a839571d4e39ee2d8184528519de281
DaniloMedeiiros <daniilomedeiiros04@gmail.com>
2024-05-15 15:57:00 -0300

    Parte principal quase pronta

--- Landpage projeto/script.js
+++
@@ -1,9 +0,0 @@
-function showServicos() {
-    document.getElementById('orcamentoBox').style.transform = 'translateX(300px)';
-    document.getElementById('servicosBox').style.transform = 'translateX(300px)';
-}
-
-function showOrcamento() {
-    document.getElementById('orcamentoBox').style.transform = 'translateX(0)';
-    document.getElementById('servicosBox').style.transform = 'translateX(0)';
-}
